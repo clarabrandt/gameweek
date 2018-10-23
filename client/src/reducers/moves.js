da1b3aeea7f6663defcd871435d343f41a6bbd42
clarabrandt <mariaclarabrandt@gmail.com>
2018-10-23 15:36:40 +0200

    allowedMovesAction

--- client/src/reducers/moves.js
+++ client/src/reducers/moves.js
@@ -1,19 +1,31 @@
-import {MOVE_PLAYER} from '../actions/games'
+import {PLAYER_MOVE} from '../actions/games'
+import {OPPONTENT_MOVE} from '../actions/games'
 
-const initialState = [{
-  position: {x : 5, y: 5},
-  vector: {x: 0, y: 0},
-}];
+
+const initialState = {
+  allowedMoves: [],
+  pastMovesPlayer: [{position: {x : 5, y: 5},
+    vector: {x: 0, y: 0}}],
+  pastMovesOpponent: [{position: {x : 6, y: 5},
+    vector: {x: 0, y: 0}}],
+  
+};
 
 
 const moves = (state=initialState, action={}) => {
   switch (action.type) {
-    case MOVE_PLAYER:
-    return [...state, {
+    case OPPONTENT_MOVE:
+    return {...state,
+      pastMovesOpponent: [...state.pastMovesOpponent, {...payload.move}] ,
+      allowedMoves: [...payload.allowedMoves] 
+    }
+
+    case PLAYER_MOVE:
+    return {...state, pastMovesPlayer: {
       position: action.payload,
       vector: {x: action.payload.x - state.x,
       y: action.payload.y - state.y}
-    }]
+    }}
     default:
       return state
   }
