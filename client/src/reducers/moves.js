1e0f49cd80fbc7b0d9d4d5def676151b17c01493
VosKoen <Koen82@gmail.com>
2018-10-24 11:10:12 +0200

    small correction

--- client/src/reducers/moves.js
+++ client/src/reducers/moves.js
@@ -1,60 +1,72 @@
-import {MOVE_PLAYER} from '../actions/games'
-import {OPPONENT_MOVE} from '../actions/games'
-
+import { MOVE_PLAYER } from "../actions/games";
+import { OPPONENT_MOVE } from "../actions/games";
 
 const initialState = {
   allowedMoves: [],
-  pastMovesPlayer: [{
-    position: {x : 5, y: 5},
-    vector: {x: 0, y: 0}
-  }],
-  pastMovesOpponent: [{
-    position: {x : 6, y: 5},
-    vector: {x: 0, y: 0}
-  }],
+  pastMovesPlayer: [
+    {
+      position: { x: 5, y: 5 },
+      vector: { x: 0, y: 0 }
+    }
+  ],
+  pastMovesOpponent: [
+    {
+      position: { x: 6, y: 5 },
+      vector: { x: 0, y: 0 }
+    }
+  ]
 };
 
-
-
-const moves = (state=initialState, action={}) => {
-  
-const vectorPlayer = {
-  x: action.payload.x - state.pastMovesPlayer[state.pastMovesPlayer.length-1].position.x,
-  y: action.payload.y - state.pastMovesPlayer[state.pastMovesPlayer.length-1].position.y
-}
-
-const vectorOpponent = {
-  x: action.payload.x - state.pastMovesOpponent[state.pastMovesOpponent.length-1].position.x,
-  y: action.payload.y - state.pastMovesOpponent[state.pastMovesOpponent.length-1].position.y
-}
-
-  
+const moves = (state = initialState, action = {}) => {
   switch (action.type) {
     case OPPONENT_MOVE:
-    return  {...state, 
-      pastMovesOpponent: [...state.pastMovesOpponent, {
-        position: {...action.payload.position},
-        vector: vectorOpponent
-      }],
-      allowedMoves: [...action.payload.allowedMoves]
-    }
+      const vectorOpponent = {
+        x:
+          action.payload.position.x -
+          state.pastMovesOpponent[state.pastMovesOpponent.length - 1].position
+            .x,
+        y:
+          action.payload.position.y -
+          state.pastMovesOpponent[state.pastMovesOpponent.length - 1].position.y
+      };
+
+      return {
+        ...state,
+        pastMovesOpponent: [
+          ...state.pastMovesOpponent,
+          {
+            position: { ...action.payload.position },
+            vector: vectorOpponent
+          }
+        ],
+        allowedMoves: [...action.payload.allowedMoves]
+      };
 
     case MOVE_PLAYER:
-    return {
-      ...state, 
-      pastMovesPlayer: [
-        ...state.pastMovesPlayer,
-        {
-          position: action.payload,
-          vector: {
-            vectorPlayer
-          }
-        }
-      ]
-    }
-    default:
-      return state
-  }
-}
+      const vectorPlayer = {
+        x:
+          action.payload.x -
+          state.pastMovesPlayer[state.pastMovesPlayer.length - 1].position.x,
+        y:
+          action.payload.y -
+          state.pastMovesPlayer[state.pastMovesPlayer.length - 1].position.y
+      };
 
-export default moves
+      return {
+        ...state,
+        pastMovesPlayer: [
+          ...state.pastMovesPlayer,
+          {
+            position: action.payload,
+            vector: {
+              vectorPlayer
+            }
+          }
+        ]
+      };
+    default:
+      return state;
+  }
+};
+
+export default moves;
