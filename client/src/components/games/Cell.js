9aa160742a23cd322d555c5c8b16ed883380cfd3
VosKoen <Koen82@gmail.com>
2018-10-25 17:49:33 +0200

    We have a track!

--- client/src/components/games/Cell.js
+++ client/src/components/games/Cell.js
@@ -5,14 +5,14 @@ class Cell extends Component {
   render() {
     const { rowIndex, cellIndex } = this.props;
     
+    if(!this.props.game.board[rowIndex][cellIndex].isOnTrack)
+    return <div className="button off-track">  </div>;
 
     if (
       this.props.game.allowedMoves.find(
         move => move.x === rowIndex && move.y === cellIndex
       )
     )
-
-
       return (
         <div 
           className= "button button-active"
